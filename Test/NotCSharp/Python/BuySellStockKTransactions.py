4139208470daba5d4762c081b6b6518671231fe4
abhisha <abhisha@microsoft.com>
2017-10-30 00:36:40 -0700

    more soln

---
+++ Test/NotCSharp/Python/BuySellStockKTransactions.py
@@ -0,0 +1,38 @@
+# https://www.youtube.com/watch?v=oDhu5uGq_ic
+def buy_sell(k, stocks):
+    len_stocks = len(stocks)
+    arr = [[None] * len_stocks for x in range(k + 1)]
+
+    for i in range(k + 1):
+        for j in range(len_stocks):
+            # i == 0 means that you are not allowed to make any transactions
+            # - if this is the case, you can never make profit, hence arr[0][j] = 0
+            # j == 0 means that you are at the starting day, you dont have any stock price of the past
+            # this means you cannot do any trading, ie - you can buy only, and NOT sell, which means profit = 0
+            if i == 0 or j == 0:
+                arr[i][j] = 0
+                continue
+
+            maxi = 0
+            for m in range(j):
+                # m is the number of days from 0 to j-1
+                # if you decide to sell on day j for a stock you bought on day m, then profit from that sale is
+                # (price on day j - price on day m) + your earlier state [without making this transaction on day m]
+                x = stocks[j] - stocks[m] + arr[i-1][m]
+                # max always takes in an iterable! remember - hence we give a list below
+                # do max between earlier max, x and the result of doing no transaction arr[i][j-1]
+                # (same state as j-1 th day)
+                maxi = max([arr[i][j-1], x, maxi])
+            arr[i][j] = maxi
+            # if your profit increased on that day - ie - you made a transaction, else you would be in prev state
+            if maxi > int(arr[i][j-1]) and arr[i][j] != arr[i-1][j]:
+                print("Bought stock on day {}".format(j-1))
+                print("Sold stock on day {}".format(j))
+
+    # bottom right cell is the answer for upto k transactions on day len_stocks
+    return arr[k][len_stocks - 1]
+
+print(buy_sell(3, [2, 5, 7, 1, 4, 3, 1, 3]))
+print(buy_sell(3, [1, 1, 1, 1, 1, 1, 1, 1]))
+print(buy_sell(3, [1, 2, 3, 4, 5, 6]))
+print(buy_sell(3, list(reversed([1, 2, 3, 4, 5, 6]))))
---
+++ Test/NotCSharp/Python/OptimalStrategyGame.py
@@ -0,0 +1,51 @@
+# https://www.youtube.com/watch?v=WxpIHvsu1RI
+# this problem is very similar to partition problem
+
+'''
+the second player is attempting to use the same optimal strategy as player one. So, after player one picks from the
+left most or right most entry, player two will have the opportunity to attempt to maximize their return.
+
+The values stored in the array are are stored in pairs where the first value in the pair is the maximum
+value that the person currently picking can get and the second value is the maximum value that the
+person going after can get. Since it's player two's turn at that point,
+the player will naturally grab the first value in the pair and the player one, who will be the next one
+to go will be forced to grab the second value in the pair.﻿
+'''
+
+
+def optimal_strategy(arr):
+    len_arr = len(arr)
+    res = [[None] * len_arr for x in range(len_arr)]
+
+    l = 1
+    for i in range(len_arr):
+        for j in range(len_arr):
+            if i == j:
+                res[i][j] = (arr[i], 0)
+
+    while l <= len_arr:
+        i = 0
+        j = l
+        while j < len_arr:
+            left = res[i][j-1]
+            bottom = res[i+1][j]
+            # understand this! Very important and subtle point
+            # first is got by either
+            # choosing starting element (i) and then choosing the best you can do with [i+1, j] (exclude the ith)
+            # OR choosing the ending element (j) and then choosing the best you can do with [i, j-1] (exclude the jth)
+            first = max([arr[i] + bottom[1], arr[j] + left[1]])
+            second = sum(arr[i:j+1]) - first
+            res[i][j] = (first, second)
+            i += 1
+            j += 1
+        l += 1
+
+    # return top right element - ie - 0 to entire length
+    return res[0][len_arr - 1]
+
+print(optimal_strategy([3, 9, 1, 2]))
+print(optimal_strategy([5, 3, 7, 10]))
+print(optimal_strategy([8, 15, 3, 7]))
+print(optimal_strategy([1, 1]))
+print(optimal_strategy([1]))
+print(optimal_strategy([5, 4, 1, 3, 6, 2]))
