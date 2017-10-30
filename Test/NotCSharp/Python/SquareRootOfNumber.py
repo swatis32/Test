2f1102c5cc1ba1703bd4d5daff22e8fb89deccb8
abhisha <abhisha@microsoft.com>
2017-10-30 16:47:21 -0700

    more soln

---
+++ Test/NotCSharp/Python/SquareRootOfNumber.py
@@ -0,0 +1,54 @@
+# This uses newtonian method
+# There is an alternate way using Binary search, however, that doesnt give the correct answer
+# - it gives nearest smallest integer as square root
+'''
+public int mySqrt(int x)
+{
+    int left = 1, right = x;
+    while (left <= right)
+    {
+        // NOTE WE NEED TO TYPECAST MID TO INT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        int mid = (left + right) / 2;
+        // The below condition is same as mid * mid == x
+        // Writing like this to avoid overflow, mid * mid may lead to overflow
+        // x/mid will NEVER lead to overflow
+        if (mid == x / mid)
+        {
+            return mid;
+        }
+        // The below condition is same as mid * mid > x
+        // Writing like this to avoid overflow, mid * mid may lead to overflow
+        // x/mid will NEVER lead to overflow
+        else if (mid < x / mid)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid - 1;
+        }
+    }
+    return right;
+}
+'''
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        r = x
+        prev_r = r
+        while r * r > x:
+            prev_r = r
+            # The idea behind this is simple
+            # https://www.youtube.com/watch?v=2158QbsunA8
+            r = (r + x / r) / 2
+            # this is done to avoid it from going into infinite loop, try removing this and doing sqrt(5)
+            if prev_r == r:
+                break
+        return r
+
+
+s = Solution()
+print(s.mySqrt(5))
