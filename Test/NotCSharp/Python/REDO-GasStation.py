c9933a1b570b7a1de16ffbd0cd5e1eaed5e52e49
abhisha <abhisha@microsoft.com>
2018-01-21 00:46:29 -0800

    more soln

---
+++ Test/NotCSharp/Python/REDO-GasStation.py
@@ -0,0 +1,44 @@
+# https://discuss.leetcode.com/topic/27760/possibly-the-most-easiest-approach-o-n-one-variable-python
+'''
+The algorithm is pretty easy to understand.
+Imagine we take a tour around this circle, the only condition that we can
+complete this trip is to have more fuel provided than costed in total. That's what the first loop does.
+
+If we do have more fuel provided than costed,
+that means we can always find a start point around this circle that we could complete
+the journey with an empty tank. Hence, we check from the beginning of the array,
+if we can gain more fuel at the current station, we will maintain the start point, else,
+which means we will burn out of oil before reaching to the next station, we will start over at the next station.
+'''
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        lengas = len(gas)
+        lencost = len(cost)
+        if lengas != lencost:
+            return -1
+        if lengas is 0:
+            return -1
+
+        gasleft = 0
+        for i in range(lengas):
+            gasleft += gas[i] - cost[i]
+
+        # we wont have enough gas if we went through all the stops
+        if gasleft < 0:
+            return -1
+
+        start = 0
+        gasleft = 0
+        # we will have gasleft > 0 at this point, now we just need to find start
+        for i in range(0, lengas):
+            gasleft += gas[i] - cost[i]
+            if gasleft < 0:
+                gasleft = 0
+                # why do we claim that start = i+1 will be the best position to start if gasleft < 0
+                start = i + 1
+        return start
