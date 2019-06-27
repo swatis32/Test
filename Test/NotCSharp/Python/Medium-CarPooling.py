1d8961e45c4be08d0113c3e1875c019e6565c3ac
Abhi Sharma <aus2101@github.com>
2019-06-27 10:04:05 -0700

    more soln

--- Test/NotCSharp/Python/Medium-CarPooling.py
+++ Test/NotCSharp/Python/Medium-CarPooling.py
@@ -7,22 +7,23 @@ class Solution(object):
         :rtype: bool
         """
         
-        # people in location (pil) = # of people at each km that the car will need carry 
+        # people in location (pil) = # of people at each km that the car will need carry if there's a stop, default is 0 
         # 1001 as max start and end index can be 1000
         # 0 <= trips[i][1] < trips[i][2] <= 1000
 
         pil = [0] * 1001
         for t in trips:
-            #1 is the start index of trip t
-            #2 is the end index of trip t
-            pil[t[1]] += t[0] #add number of people needed at start index
+            #1 is the start index of trip
+            #2 is the end index of trip
+            pil[t[1]] += t[0] #add cap at loc at start index as people are loading
             # at any point people at a location cant exceed capacity
             if pil[t[1]] > capacity:
                 return False
-            pil[t[2]] -= t[0] #subtract number of people needed at end index
+            pil[t[2]] -= t[0] #subtract cap at loc at end index as people are getting off
         
+        tmpcap = 0
         for i in pil:
-            if capacity < 0:
+            if tmpcap > capacity:
                 return False
-            capacity -= i
+            tmpcap += i
         return True
