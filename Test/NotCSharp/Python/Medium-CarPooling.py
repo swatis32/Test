c722d4248d62a585bc652bb97c362ee944cec4ef
Abhi Sharma <aus2101@github.com>
2019-06-27 09:17:24 -0700

    more soln

---
+++ Test/NotCSharp/Python/Medium-CarPooling.py
@@ -0,0 +1,28 @@
+# based off this: https://leetcode.com/problems/car-pooling/discuss/317611/C%2B%2BJava-O(n)-Thousand-and-One-Stops
+class Solution(object):
+    def carPooling(self, trips, capacity):
+        """
+        :type trips: List[List[int]]
+        :type capacity: int
+        :rtype: bool
+        """
+        
+        # people in location (pil) = # of people at each km that the car will need carry 
+        # 1001 as max start and end index can be 1000
+        # 0 <= trips[i][1] < trips[i][2] <= 1000
+
+        pil = [0] * 1001
+        for t in trips:
+            #1 is the start index of trip t
+            #2 is the end index of trip t
+            pil[t[1]] += t[0] #add number of people needed at start index
+            # at any point people at a location cant exceed capacity
+            if pil[t[1]] > capacity:
+                return False
+            pil[t[2]] -= t[0] #subtract number of people needed at end index
+        
+        for i in pil:
+            if capacity < 0:
+                return False
+            capacity -= i
+        return True
