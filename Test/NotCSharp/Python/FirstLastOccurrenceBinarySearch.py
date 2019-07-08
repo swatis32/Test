ee8718d62776b750c73d91fa3a7a0536c8a51fa0
Abhi Sharma <aus2101@github.com>
2019-07-07 18:17:46 -0700

    more soln

--- Test/NotCSharp/Python/FirstLastOccurrenceBinarySearch.py
+++ Test/NotCSharp/Python/FirstLastOccurrenceBinarySearch.py
@@ -34,3 +34,11 @@ def lastbin(arr, target):
     return "last index is {0} where we find element {1}".format(result, target)
 
 print(lastbin([1,2,3,3,3,3,4,5,6,7], 3))
+
+'''
+One interesting application of this problem is that we can find how many counts of a target number are in an array
+For example, we can find the number of times 3 occurs in the above array in log n time - which is better than o(N) time
+ans = print(lastbin([1,2,3,3,3,3,4,5,6,7], 3)) - print(firstbin([1,2,3,3,3,3,4,5,6,7], 3)) + 1
+so we're basically doing, ans = lastindex - firstindex + 1 (5-2+1 = 4). There are 4 counts of number 3
+
+'''
