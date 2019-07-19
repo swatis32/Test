8cdd02a902657c381e1e6e3d45061932d1d9169f
Abhi Sharma <aus2101@github.com>
2019-07-19 08:34:02 -0700

    more soln

--- Test/NotCSharp/Python/CombinationSum3.py
+++ Test/NotCSharp/Python/CombinationSum3.py
@@ -14,14 +14,18 @@ class Solution(object):
         return self.comb
     
     def helper(self, k, n, arr):
+        # sum cant be <0 and we need to stick to array length of k
         if n < 0 or len(arr) > k:
             return
         
         if len(arr) == k and n == 0 and list(arr) not in self.comb:
+            # its important to add list(arr) and not arr. Why?
+            # because list(arr) creates a new list, and just arr is a reference
             self.comb.append(list(arr))
             return
         
         for i in range(1,10):
+            #  cant have duplicates
             if i not in arr:
                 arr.add(i)
                 self.helper(k, n-i, arr)
